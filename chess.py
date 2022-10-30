dc48c5460064e8c613e436a8d6cf0c5ddb2122d6
Потапов Даниил <113824271+potopik@users.noreply.github.com>
2022-10-31 00:00:46 +0500

    chess with log.py

--- chess.py
+++ chess.py
@@ -1,20 +1,34 @@
-while True:  # Проверка на ввод данных
+import logging
+logging.basicConfig(
+    filename="sampling.log",
+    level=logging.INFO,
+    format="%(asctime)s - [%(levelname)s] - %(app)s - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s",
+)
+logger = logging.getLogger("LOGGER")
+logger = logging.LoggerAdapter(logger, {"app": "тестовое приложение"})
+
+while True:  # Проверка на ввод данных и соответственно ввод данных
+    logger.info("Программа стартует")
     try:
         horizont = int(input('Введите координату клетки фигуры по горизонтали: '))
         if horizont > 8 or horizont < 0:
             print('Введены некорректные данные. Попробуйте снова.')
+            logger.error('Введены некорректные данные. Попробуйте снова.')
             continue
         vertical = int(input('Введите координату клетки фигуры по вертикали: '))
         if vertical > 8 or vertical < 0:
             print('Введены некорректные данные. Попробуйте снова.')
+            logger.error('Введены некорректные данные. Попробуйте снова.')
             continue
         attackhorizont = int(input('Введите координату атакуемой клетки по горизонтали: '))
         if attackhorizont > 8 or attackhorizont < 0:
             print('Введены некорректные данные. Попробуйте снова.')
+            logger.error('Введены некорректные данные. Попробуйте снова.')
             continue
         attackvertical = int(input('Введите координату атакуемой клетки по вертикали: '))
         if attackvertical > 8 or attackvertical < 0:
             print('Введены некорректные данные. Попробуйте снова.')
+            logger.error('Введены некорректные данные. Попробуйте снова.')
             continue
         figure = int(input('''Какую фигуру вы хотите использовать?
         1 - Ферзь
@@ -24,20 +38,26 @@ while True:  # Проверка на ввод данных
         Ваш выбор: '''))
         if figure > 4 or figure < 0:
             print('Введены некорректные данные. Попробуйте снова.')
+            logger.error('Введены некорректные данные. Попробуйте снова.')
             continue
     except ValueError:
         print('Введены некорректные данные. Попробуйте снова.')
+        logger.error('Введены некорректные данные. Попробуйте снова.')
         continue
-
+    fig = {1: 'ферзь', 2: 'ладья', 3: 'слон', 4: 'конь'}
+    logger.info(f'Добавлены координаты ({horizont}; {vertical}) and ({attackhorizont}; {attackvertical}) и выбрана фигура - {fig[figure]}')
     # Проверка на совпадение цвета полей
     if (horizont + vertical) % 2 == (attackhorizont + attackvertical) % 2:
         print('Они одного цвета -', end=' ')
         if (horizont + vertical) % 2 == 0:
             print('белого')
+            logger.info('output: Они одного белого цвета')
         else:
             print('черного')
+            logger.info('output: Они одного черного цвета')
     else:
         print('Поля не одного цвета')
+        logger.info('output: Они не одного цвета')
 
     # Расстояние по горизонтали и вертикали
     dx = abs(horizont - attackhorizont)
@@ -47,22 +67,32 @@ while True:  # Проверка на ввод данных
     if figure == 1:     # Ферзь
         if horizont == attackhorizont or vertical == attackvertical or dx == dy:
             print(f'Ферзь угрожает полю ({attackhorizont}; {attackvertical})')
+            logger.info(f'Ферзь угрожает полю ({attackhorizont}; {attackvertical})')
         else:
             print(f'Ферзь не угрожает полю ({attackhorizont}; {attackvertical})')
+            logger.info(f'Ферзь не угрожает полю ({attackhorizont}; {attackvertical})')
             print(f'Чтобы за два хода попасть в это поле необходимо встать на поле ({attackhorizont}; {vertical})')
+            logger.info(f'Чтобы за два хода попасть в это поле необходимо встать на поле ({attackhorizont}; {vertical})')
     elif figure == 2:   # Ладья
         if horizont == attackhorizont or vertical == attackvertical:
             print(f'Ладья угрожает полю ({attackhorizont}; {attackvertical})')
+            logger.info(f'Ладья угрожает полю ({attackhorizont}; {attackvertical})')
         else:
             print(f'Ладья не угрожает полю ({attackhorizont}; {attackvertical})')
+            logger.info(f'Ладья не угрожает полю ({attackhorizont}; {attackvertical})')
             print(f'Чтобы за два хода попасть в это поле необходимо встать на поле ({attackhorizont}; {vertical})')
+            logger.info(f'Чтобы за два хода попасть в это поле необходимо встать на поле ({attackhorizont}; {vertical})')
     elif figure == 3:   # Слон
         if dx == dy:
             print(f'Слон угрожает полю ({attackhorizont}; {attackvertical})')
+            logger.info(f'Слон угрожает полю ({attackhorizont}; {attackvertical})')
+
         else:
             print(f'Слон не угрожает полю ({attackhorizont}; {attackvertical})')
+            logger.info(f'Слон не угрожает полю ({attackhorizont}; {attackvertical})')
             if (horizont + vertical) % 2 != (attackhorizont + attackvertical) % 2:
                 print(f'Слон никаким образом не может угрожать полю ({attackhorizont}; {attackvertical})')
+                logger.info(f'Слон никаким образом не может угрожать полю ({attackhorizont}; {attackvertical})')
             else:
                 m0 = attackhorizont
                 n0 = attackvertical
@@ -103,9 +133,13 @@ while True:  # Проверка на ввод данных
                         n1 = n0
                         break
                 print(f'Чтобы за два хода попасть в это поле необходимо встать на поле ({m1}; {n1})')
+                logger.info(f'Чтобы за два хода попасть в это поле необходимо встать на поле ({m1}; {n1})')
     else:   # Конь
         if abs(dx - dy) == 1:
             print(f'Конь угрожает полю ({attackhorizont}; {attackvertical})')
+            logger.info(f'Конь угрожает полю ({attackhorizont}; {attackvertical})')
         else:
             print(f'Конь не угрожает полю ({attackhorizont}; {attackvertical})')
+            logger.info(f'Конь не угрожает полю ({attackhorizont}; {attackvertical})')
     break
+logger.info("Программа завершила работу")
